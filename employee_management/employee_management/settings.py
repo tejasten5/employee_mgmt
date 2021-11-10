50d7581290c291fa2139a89e2fe6761f066d762f
Tejas <tejas.tendulkar@trigensoft.com>
2021-11-10 20:28:14 +0530

    deployment

--- employee_management/employee_management/settings.py
+++ employee_management/employee_management/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-@k36ym9a=0$u)&*1-qdx_+es8ewng%fd#rfhd-i_ry+pgx&g6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
